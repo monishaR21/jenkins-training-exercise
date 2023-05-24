00c9bbc6dc74ebb37f918c3c3c79474fe21e275a
monishaR21 <62681450+monishaR21@users.noreply.github.com>
2023-05-24 11:40:54 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
 	    public static void main(String args[]){  
-		         System.out.println("Hello Java monisha R hello");  
+		         System.out.println("Hello Java monisha R hello and added the webhook");  
 			     }  
 }  
